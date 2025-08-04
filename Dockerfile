337b1b25c1866c4dd7829c891c12021c97d03058
callistus <callistus406@gmail.com>
2025-08-04 22:56:00 +0100

    feat Added docker

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:current-alpine3.22
+
+
+WORKDIR /app
+
+
+COPY package.json package-lock.json /app/
+
+RUN npm install
+
+
+
+COPY . ./
+
+
+EXPOSE 4000
+
+
+CMD [ "npm","run", "dev" ]
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { logger } from "./middleware/logger.middleware";
 import { mongoConnection } from "./config/db.connection";
 import { handleCustomError } from "./middleware/errorHandler.midleware";
 import router from "./routes";
+import { limiter } from "./middleware/reate-limit";
+import helmet from "helmet";
+import cors from "cors";
+
 // import dotenv from "dotenv";
 // dotenv.config();
 const app = express();
@@ -11,10 +15,19 @@ const app = express();
 const port = 4000;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5501",
+    methods: "HEAD",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
+// app.use(logger);
+app.use(helmet());
+app.use(limiter);
 
-app.use(logger);
-
-app.use("/api/v1",router);
+app.use("/api/v1", router);
 // app.use("/api/v2",router);
 app.use(handleCustomError);
 
